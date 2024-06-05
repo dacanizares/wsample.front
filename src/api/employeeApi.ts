a807f96db5c87b8b2ac022bc8aeb3d41ba4d30ea
dacanizares <dacanizares@outlook.com>
2024-06-04 22:21:04 -0500

    Added employee api (WIP)

---
+++ src/api/employeeApi.ts
@@ -0,0 +1,17 @@
+'use server'
+
+import { executeFetch, executeFetchNoCache } from "@/common/fetchUtils"
+import { API_URL } from "@/common/constants"
+import Employee from "@/models/Employee";
+
+const EMPLOYEES_API = `${API_URL}/employee`
+
+export async function FindEmployees(): Promise<Employee[]> {
+  const res = await executeFetchNoCache(`${EMPLOYEES_API}`);
+  if (res.status !== 200) {
+    throw Error(`Expected status 200 but received ${ res.status }`);
+  } else {
+    const data = await res.json();
+    return data;
+  }
+}
