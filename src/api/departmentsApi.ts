f2712345749f298be6599ac5534e82101c9ce8ff
dacanizares <dacanizares@outlook.com>
2024-06-04 22:04:14 -0500

    Updated departments api

--- src/api/departmentsApi.ts
+++ src/api/departmentsApi.ts
@@ -2,17 +2,16 @@
 
 import { executeFetch } from "@/common/fetchUtils"
 import { API_URL } from "@/common/constants"
+import Department from "@/models/Department";
 
 const DEPARTMENTS_URL = `${API_URL}/department`
 
-export async function FindDepartments(): Promise<void> {
+export async function FindDepartments(): Promise<Department[]> {
   const res = await executeFetch(`${DEPARTMENTS_URL}`);
   if (res.status !== 200) {
-    console.log('ERROR');
+    throw Error(`Expected status 200 but received ${ res.status }`);
+  } else {
+    const data = await res.json();
+    return data;
   }
-  
-  console.log(res);
-  const data = await res.json();
-  console.log(data);
-  return data;
 }
