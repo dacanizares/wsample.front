29f03d36b96973b573123cfb3f5857096dbde3e3
dacanizares <dacanizares@outlook.com>
2024-06-05 11:13:36 -0500

    Improved responsive avatars

--- src/components/EmployeeSingle.tsx
+++ src/components/EmployeeSingle.tsx
@@ -11,13 +11,13 @@ interface EmployeeSingleProps {
 export default function EmployeeSingle({ employee }: EmployeeSingleProps) {
   return (
     <div className="relative flex flex-col sm:flex-row items-center border border-solid border-gray-200 bg-slate-50 rounded m-4 sm:m-0 drop-shadow-md">
-      <div className="block overflow-hidden md:w-52 h-48">
+      <div className="flex justify-center overflow-hidden w-60 h-64 md:w-52 h-48">
         <EmployeeAvatar
           active={employee.active}
           avatarUrl={employee.avatarUrl}
         />
       </div>
-      <div className="flex flex-col justify-between h-full w-full p-3 text-center sm:text-left">
+      <div className="flex flex-col justify-between md:h-full md:w-full p-3 text-center sm:text-left">
       <h2 className="font-semibold text-gray-900 mb-2">
           <EmployeeMainInfo 
             firstName={employee.firstName}
