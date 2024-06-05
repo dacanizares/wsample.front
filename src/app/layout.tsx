d9a7868810277ccf7032c82beefceebe9a392036
dacanizares <dacanizares@outlook.com>
2024-06-04 23:30:44 -0500

    Fixed debug tools

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,9 +15,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="container mx-auto">{children}</body>
-
-      { process.env.DEBUG === 'true' && <DebugResponsive />} 
+      <body className="container mx-auto">
+        {children}
+        { process.env.DEBUG === 'true' && <DebugResponsive />} 
+      </body>
     </html>
   );
 }
